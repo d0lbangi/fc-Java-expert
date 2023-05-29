7846496d58bc4933b6f42f71281e2b3eb3745909
Jeongho Park <d0lbangi13@naver.com>
2023-05-30 01:09:16 +0900

    add Variable naming conventions and variable notation for each language

---
+++ src/main/java/com/example/VariableNotation/VariableNotation.java
@@ -0,0 +1,28 @@
+package com.example.VariableNotation;
+
+public class VariableNotation {
+
+    // 파스칼표기법 -> java에서 클래스를 정의할 때, c#언어에서 변수나 클래스를 선언할 때, 파이썬 클래스
+    // Data, LocalDataTime, String, Scanner
+
+    void speedTest() {
+        int speed = 10;
+    }
+    // 카멜표기법 -> java에서 변수, 메소드를 정의할때
+
+    // 언더바표기법(스네이크표기법)-소문자 -> c, php, 파이썬
+    void speed_test() {
+
+    }
+
+    // 언더바표기법(스네이크표기법)-대문자 -> SQL문법, java에서는 상수
+    final int MAX_SPEED = 100;
+    // SELECT * FROM USER
+
+    // 헝가리언표기법
+    int iMaxSpeed;
+    String strUserName;
+
+    // 케바표기법
+    // http://www.naver.com/board-list
+}
